d8a1db0094b085c3901235d6396f02ec5474b73b
WhispCat <138867093+whispcat@users.noreply.github.com>
2024-11-02 18:17:17 -0700

    fix anki-beta cask syntax

--- Casks/anki-beta.rb
+++ Casks/anki-beta.rb
@@ -3,9 +3,9 @@ cask "anki-beta" do
 
   version "24.10"
   
-  sha256 arm: "7a545c3976b2aa9228f740597ea856bfca709a72f8a554afd4427e5380550ba8"
+  sha256 arm: "7a545c3976b2aa9228f740597ea856bfca709a72f8a554afd4427e5380550ba8",
          intel: "61346181ccf4a15643c8142f61ec01a4c56285d2cd654f2c489307ae0384d0da"
-  url "https://github.com/ankitects/anki/releases/download/#{version.concat("rc")}/anki-#{version}-mac-#{arch}-qt6.dmg"
+  url "https://github.com/ankitects/anki/releases/download/#{version.concat("rc")}/anki-#{version}-mac-#{arch}-qt6.dmg",
       verified: "github.com/ankitects/anki/"
 
   name "Anki newest version possible, including beta and rc"
