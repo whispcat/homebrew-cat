645361e002412a4ce428a0f18c5edda4048cc2ae
WhispCat <138867093+whispcat@users.noreply.github.com>
2025-01-27 01:00:04 -0800

    update anki beta version

--- Casks/anki-beta.rb
+++ Casks/anki-beta.rb
@@ -1,13 +1,13 @@
 cask "anki-beta" do
   arch arm: "apple", intel: "intel"
 
-  version "24.11"
-  sha256 arm:   "004a72ad9050fb4148f42c7009a70695b303319ede79b0513e443436cb6e6e79",
-         intel: "77de257e4d695098319380f2970f9f0b6aadf307d127bb494189be0a1a752799"
+  version "25.01rc1"
+  sha256 arm:   "c5972dfa06c41ae04a3567b5dadacc66f948b29fec5cffb161618bcf427ca2fc",
+         intel: "556d08b7c6c51dfa50879329d5e624cdd5efbef7753f57e7f4d383483c5d521d"
 
-  url "https://github.com/ankitects/anki/releases/download/#{version}/anki-#{version}-mac-#{arch}-qt6.dmg",
+  url "https://github.com/ankitects/anki/releases/download/#{version}/anki-#{version.sub("rc1", "")}-mac-#{arch}-qt6.dmg",
       verified: "github.com/ankitects/anki/"
-  name "Anki newest version possible, including beta and rc"
+  name "Anki Newest"
   desc "Memory training application"
   homepage "https://apps.ankiweb.net/"
 
