baad54e7b6d3983c13e1cb8e44befe2a2870e485
WhispCat <138867093+whispcat@users.noreply.github.com>
2025-04-26 23:54:55 -0700

    fix anki-beta

--- Casks/anki-beta.rb
+++ Casks/anki-beta.rb
@@ -2,7 +2,7 @@ cask "anki-beta" do
   arch arm: "apple", intel: "intel"
 
   version "25.05b1"
-  sha256 arm:   "679aa4e73bebd13e3b4c06f743fe33c34c1396a87be2bfa982413846a1726634 ",
+  sha256 arm:   "679aa4e73bebd13e3b4c06f743fe33c34c1396a87be2bfa982413846a1726634",
          intel: "3bc9dd9b5121c9207dda8c08249d090eaa9897b2fc342d32ef76b03a33cac6b1"
 
   url "https://github.com/ankitects/anki/releases/download/#{version}/anki-#{version.sub("b1", "")}-mac-#{arch}-qt6.dmg",
